855a63b655e0fb52473f9fcc5eec3a6a0522bcde
KollaNithinReddy <157342778+KollaNithinReddy@users.noreply.github.com>
2024-04-24 09:15:11 +0530

    main.tf

--- Terraform-task/modules/lambda/main.tf
+++ Terraform-task/modules/lambda/main.tf
@@ -9,8 +9,7 @@ data "archive_file" "lambda_zip" {
 
 resource "aws_lambda_function" "test_lambda" {
 
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
+  
   filename      = data.archive_file.lambda_zip.output_path
   function_name = var.function_name
   role          = aws_iam_role.iam_for_lambda.arn
